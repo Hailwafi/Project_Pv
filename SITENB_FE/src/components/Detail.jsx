d3b791ed8e8e6c1dc1f7afaaf233dce4a30bcad0
unknown <hailwafiputra.com>
2024-10-24 06:04:21 +0700

    update

--- SITENB_FE/src/components/Detail.jsx
+++ SITENB_FE/src/components/Detail.jsx
@@ -24,14 +24,15 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full sm:w-3/4 md:w-2/3 lg:w-1/2 max-h-[80vh] overflow-y-auto mt-10">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-3xl max-h-[80vh] overflow-y-auto mt-10">
         <div className="relative isolate mt-10 lg:px-8">
           <form>
             <div className="space-y-12">
               <div className="border-b border-gray-900/10 pb-12">
-                <div className="mt-10 grid grid-cols-3 gap-x-6 gap-y-8 sm:grid-cols-9">
+                <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-8">
+                  
                   {/* Tanggal Field */}
-                  <div className="sm:col-span-3">
+                  <div>
                     <label htmlFor="date" className="block text-sm font-medium leading-6 text-gray-900">
                       Tanggal:
                     </label>
@@ -45,7 +46,7 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
                   </div>
 
                   {/* Nama Lengkap */}
-                  <div className="sm:col-span-3">
+                  <div>
                     <label htmlFor="nama_lengkap" className="block text-sm font-medium leading-6 text-gray-900">
                       Nama Lengkap
                     </label>
@@ -62,7 +63,7 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
                   </div>
 
                   {/* Email */}
-                  <div className="sm:col-span-3">
+                  <div>
                     <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                       Email
                     </label>
@@ -79,17 +80,17 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
                   </div>
 
                   {/* Jabatan */}
-                  <div className="sm:col-span-3">
+                  <div>
                     <label htmlFor="jabatan" className="block text-sm font-medium leading-6 text-gray-900">
                       Jabatan
                     </label>
                     <div className="mt-2">
-                      {tickets.Jabatan ? (
+                      {tickets.jabatan ? (
                         <div
                           id="jabatan"
                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"
                         >
-                          {tickets.Jabatan}
+                          {tickets.jabatan}
                         </div>
                       ) : (
                         <div
@@ -103,7 +104,7 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
                   </div>
 
                   {/* Nomor Induk Pegawai */}
-                  <div className="sm:col-span-3">
+                  <div>
                     <label htmlFor="nomor_induk_pegawai" className="block text-sm font-medium leading-6 text-gray-900">
                       Nomor Induk Pegawai
                     </label>
@@ -126,8 +127,8 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
                     </div>
                   </div>
 
-    {/* kategori */}
-    <div className="sm:col-span-3">
+                  {/* Kategori */}
+                  <div>
                     <label htmlFor="kategori" className="block text-sm font-medium leading-6 text-gray-900">
                       Kategori
                     </label>
@@ -139,16 +140,15 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
                         readOnly
                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"
                       >
-      {formatString(tickets.kategori)}
-
+                        {formatString(tickets.kategori)}
                       </div>
                     </div>
                   </div>
 
-     {/* Jenis Tiket */}
-     <div className="sm:col-span-3">
+                  {/* Jenis Tiket */}
+                  <div>
                     <label htmlFor="tiket" className="block text-sm font-medium leading-6 text-gray-900">
-                    Jenis Tiket
+                      Jenis Tiket
                     </label>
                     <div className="mt-2">
                       <input
@@ -162,19 +162,57 @@ const Detail = ({ tickets, onClose, staffList, onAssignTask }) => {
                     </div>
                   </div>
 
+                  {/* Foto */}
+                  <div>
+                    <label htmlFor="foto" className="block text-sm font-medium leading-6 text-gray-900">
+                      Foto
+                    </label>
+                    <div className="mt-2">
+                      <input
+                        id="foto"
+                        name="foto"
+                        type="text"
+                        value={tickets.unggah_file}
+                        readOnly
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"
+                      />
+                    </div>
+                  </div>
 
+                  {/* Deskripsi */}
+                  <div className="col-span-full">
+                    <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
+                      Deskripsi
+                    </label>
+                    <div className="mt-2">
+                      <textarea
+                        id="description"
+                        name="description"
+                        rows={3}
+                        placeholder="Deskripsi"
+                        value={tickets.deskripsi}
+                        readOnly
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
 
                 </div>
               </div>
             </div>
           </form>
+          
+          {/* Assign Task Form */}
           <AssignTaskForm
-            ticketId={tickets.id} 
-            onAssignTask={handleAssignTask} 
+            ticketId={tickets.id}
+            onAssignTask={handleAssignTask}
           />
+
+          {/* Close Button */}
           <button
             onClick={onClose}
-            className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-red-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+            className="mt-4 inline-flex justify-center rounded-md border border-transparent bg-red-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+          >
             Tutup
           </button>
         </div>
